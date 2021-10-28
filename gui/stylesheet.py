bbc9a69d6284b533a795327d0b86a52d427ad4aa
lenemter <lenemter@gmail.com>
2021-10-28 21:50:47 +0900

    Some renames

--- gui/stylesheet.py
+++ gui/stylesheet.py
@@ -5,8 +5,7 @@ def get_dark_palette():
     palette = QPalette()
     palette.setColor(QPalette.Window, QColor(53, 53, 53))
     palette.setColor(QPalette.WindowText, Qt.white)
-    palette.setColor(QPalette.Disabled, QPalette.WindowText,
-                     QColor(127, 127, 127))
+    palette.setColor(QPalette.Disabled, QPalette.WindowText, QColor(127, 127, 127))
     palette.setColor(QPalette.Base, QColor(42, 42, 42))
     palette.setColor(QPalette.AlternateBase, QColor(66, 66, 66))
     palette.setColor(QPalette.ToolTipBase, Qt.white)
@@ -17,15 +16,13 @@ def get_dark_palette():
     palette.setColor(QPalette.Shadow, QColor(20, 20, 20))
     palette.setColor(QPalette.Button, QColor(53, 53, 53))
     palette.setColor(QPalette.ButtonText, Qt.white)
-    palette.setColor(QPalette.Disabled, QPalette.ButtonText,
-                     QColor(127, 127, 127))
+    palette.setColor(QPalette.Disabled, QPalette.ButtonText, QColor(127, 127, 127))
     palette.setColor(QPalette.BrightText, Qt.red)
     palette.setColor(QPalette.Link, QColor(42, 130, 218))
     palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
     palette.setColor(QPalette.Disabled, QPalette.Highlight, QColor(80, 80, 80))
     palette.setColor(QPalette.HighlightedText, Qt.white)
-    palette.setColor(QPalette.Disabled, QPalette.HighlightedText,
-                     QColor(127, 127, 127))
+    palette.setColor(QPalette.Disabled, QPalette.HighlightedText, QColor(127, 127, 127))
 
     return palette
 
@@ -33,6 +30,6 @@ def get_dark_palette():
 def get_font():
     from PyQt5.QtWidgets import QApplication
 
-    default_font = QApplication.font()
-    default_font.setPointSize(default_font.pointSize() + 1)
-    return default_font
+    font = QApplication.font()
+    font.setPointSize(font.pointSize() + 1)
+    return font
