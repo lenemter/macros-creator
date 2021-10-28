e52ded5b6b967c1275a5267907a4b3a13395ca48
lenemter <lenemter@gmail.com>
2021-10-28 21:49:40 +0900

    Fix imports

--- runner/read_file.py
+++ runner/read_file.py
@@ -4,7 +4,7 @@ from .utils import NAME_CLASS_DICT
 def read_file_xml(filepath) -> tuple:
     import xml.etree.ElementTree as ET
 
-    actions_list = []
+    actions = []
     tree = ET.parse(filepath)
     root = tree.getroot()
     settings = root.attrib
@@ -12,30 +12,15 @@ def read_file_xml(filepath) -> tuple:
         child = child
         tag = child.tag
         attrib = child.attrib
-        action = NAME_CLASS_DICT[tag.capitalize().replace('_', ' ')](**attrib)  # create action
-        actions_list.append(action)
+        action_cls = NAME_CLASS_DICT[tag.capitalize().replace('_', ' ')]
+        action = action_cls(**attrib)  # create action
+        actions.append(action)
 
     # noinspection PyTypeChecker
     settings['time_between'] = float(settings['time_between'])
 
-    return actions_list, settings
+    return actions, settings
 
 
-def read_file_db(filepath) -> tuple:
-    import sqlite3
-
-    connection = sqlite3.connect(filepath)
-    cursor = connection.cursor()
-    settings = cursor.execute('SELECT name, value FROM settings').fetchall()
-    actions_list = cursor.execute('SELECT name, parameters FROM macro').fetchall()
-    connection.close()
-
-    settings = {key: value for (key, value) in settings}
-    # noinspection PyTypeChecker
-    settings['time_between'] = float(settings['time_between'])
-
-    actions_list = []
-
-    print(f'{settings=} {actions_list=}')
-
-    return actions_list, settings
+def read_file_csv(filepath) -> tuple:
+    import csv
--- runner/write_file.py
+++ runner/write_file.py
@@ -1,55 +1,17 @@
 from .utils import NAME_CLASS_DICT
 
 
-def write_file_xml(filepath, actions_list: list, settings: dict):
+def write_file_xml(filepath, actions: list, settings: dict):
     import xml.etree.ElementTree as ET
 
     settings = {str(x): str(y) for (x, y) in settings.items()}
 
     root = ET.Element('macro', settings)
-    for action in actions_list:
+    for action in actions:
         root.append(action.xml())
     tree = ET.ElementTree(root)
     with open(filepath, mode='wb') as file:
         tree.write(file, encoding='UTF-8')
 
-
-def write_file_db(filepath, actions_list: list, settings: dict):
-    import sqlite3
-
-    connection = sqlite3.connect(filepath)
-    cursor = connection.cursor()
-    cursor.execute("""
-                    DROP TABLE IF EXISTS settings
-                    """)
-    cursor.execute("""
-                    DROP TABLE IF EXISTS macro
-                   """)
-
-    cursor.execute("""
-                    CREATE TABLE settings (
-                        name TEXT PRIMARY KEY,
-                        value TEXT
-                    )
-                    """)
-    cursor.execute("""
-                    CREATE TABLE macro (
-                        name TEXT PRIMARY KEY,
-                        parameters TEXT
-                    )
-                   """)
-
-    for (setting, value) in settings.items():
-        cursor.execute(f"""
-                        INSERT INTO settings (name, value)
-                        VALUES ("{setting}", "{value}")
-                        """)
-    for action in actions_list:
-        name = NAME_CLASS_DICT[action.name].__name__
-        cursor.execute(f"""
-                        INSERT INTO macro (name, parameters)
-                        VALUES ("{name}", "{str(action.parameters)[1:-1]}")
-                        """)
-
-    connection.commit()
-    connection.close()
+def write_file_csv(filepath, actions: list, settings: dict):
+    pass
